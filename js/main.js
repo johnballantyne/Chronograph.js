e4466ca2ed2229e9b533a2eb94cf1c570919bacb
John Ballantyne <johnballantyne@gmail.com>
2013-05-02 22:15:50 -0400

    Add auto-reset to the driver

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 document.onkeydown = keypress;
 var stopwatch;
 var timer;
+var terminated;
 
 var updateStopwatch = function() {
-    if ( stopwatch.time >= 5000 ) {
+    if ( stopwatch.time >= 10000 ) {
         stopwatch.stop();
-        stopwatch.time = Math.min( 5000, stopwatch.time );
+        terminated = true;
+        stopwatch.time = Math.min( 10000, stopwatch.time );
         document.getElementById( "bell" ).play();
     }
     document.getElementById( "stopwatch" ).innerHTML = prettyTime( stopwatch.time );
-    document.getElementById( "stopwatchprog").setAttribute( "value", ( stopwatch.time / 5000 ));
+    document.getElementById( "stopwatchprog").setAttribute( "value", ( stopwatch.time / 10000 ));
 };
 
 var updateTimer = function() {
@@ -18,12 +20,12 @@ var updateTimer = function() {
         timer.time = Math.max( 0, timer.time );
     }
     document.getElementById( "timer" ).innerHTML = prettyTime( timer.time );
-    document.getElementById( "timerprog" ).setAttribute( "value", ( timer.time / 5000 ));
+    document.getElementById( "timerprog" ).setAttribute( "value", ( timer.time / 10000 ));
 };
 
 function init () {
     stopwatch = new Stopwatch( updateStopwatch );
-    timer = new Timer( updateTimer, 5000 );
+    timer = new Timer( updateTimer, 10000 );
 }
 
 window.onload = init;
@@ -31,6 +33,11 @@ window.onload = init;
 function keypress( evt ) {
     evt = evt || window.event;
     if ( evt.keyCode == 32 ) {
+        if (terminated === true) {
+            timer.reset();
+            stopwatch.reset();
+            terminated = false;
+        }
         stopwatch.toggle();
         timer.toggle();
     }
